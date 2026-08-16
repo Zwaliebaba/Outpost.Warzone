6f2dccefbf53d9c6c987a1b4b1cff9ae90d05516
Stefan Zwaal <zwalie@live.com>
2026-08-16 17:14:31 +0200

    Testing added

---
+++ NeuronClientTest/NeuronClientTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NeuronClientTest
+{
+	TEST_CLASS(NeuronClientTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ NeuronServerTest/NeuronServerTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NeuronServerTest
+{
+	TEST_CLASS(NeuronServerTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
